faac8be9537f5c704c608b9c45b846cd2823b69d
HelloCodeMing <huanmingwong@163.com>
2015-06-03 14:35:48 +0800

    finish 2-6

--- chapter1/type_traits_ext.cc
+++ chapter1/type_traits_ext.cc
@@ -1,6 +1,7 @@
 #include <type_traits>
 #include <cassert>
 
+/* is_same metafunction */
 template <class T1, class T2>
 struct is_same {
     const static bool value = false;
@@ -11,6 +12,7 @@ struct is_same<T1, T1> {
     const static bool value = true;
 };
 
+/* make_unsigned family */
 template <class T>
 struct make_unsigned {
     typedef T type;
@@ -31,9 +33,24 @@ struct make_unsigned<long> {
     typedef unsigned long type;
 };
 
+/* result of a function */
+template <class Fn>
+struct result_of {
+    typedef Ret type;
+};
+
+template <class Ret, class Arg>
+struct result_of<Ret(Arg)> {
+    typedef Ret type;
+};
+
 int main()
 {
     bool same = is_same<unsigned int, typename make_unsigned<int>::type>::value;
     assert(same);
+    same = is_same<int*, typename result_of<int*(char&)>::type>::value;
+    assert(same);
+    same = is_same<int*, typename result_of<int&(char*)>::type>::value;
+    assert(same == false);
     return 0;
 }
