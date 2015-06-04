2907ba31147845cb4149051882938111f0d43bc6
HelloCodeMing <huanmingwong@163.com>
2015-06-04 19:21:12 +0800

    could not finish 3-6

--- chapter3/3-6.cc
+++ chapter3/3-6.cc
@@ -5,16 +5,17 @@ template <class Fn, class Arg>
 struct twice: mpl::apply<Fn, typename mpl::apply<Fn, Arg>::type> {
 };
 
-struct twice2: mpl::apply<_1, typename mpl::apply<_1, _2>::type> {
+struct twice2 {
+    template <class Fn, class Arg>
+        struct apply: mpl::apply<Fn, typename mpl::apply<Fn, Arg>::type> {
+        };
 };
 
 int main()
 {
     
-    typedef typename mpl::apply<_1, typename mpl::apply<_1, _2>::type>::type twice3;
     static_assert(std::is_same<twice<std::add_pointer<_1>, int>::type, int**>::value, "twice");
-    //static_assert(std::is_same<twice2<std::add_pointer<_1>, int>::type, int**>::value, "twice2");
-    static_assert(std::is_same<twice2::template apply<std::add_pointer<_1>, int>::type, int**>::value, "shit");
-    static_assert(std::is_same<twice3::template apply<std::add_pointer<_1>, int>::type, int**>::value, "shit");
+    
+    static_assert(std::is_same<mpl::apply<twice2, std::add_pointer<_1>, int>::type, int**>::value, "shit");
     return 0;
 }
