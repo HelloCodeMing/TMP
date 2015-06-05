341086971023a4a966ae26114b6b2cba8120be17
HelloCodeMing <huanmingwong@163.com>
2015-06-05 10:46:27 +0800

    optimize 3-7

--- chapter3/3-6.cc
+++ chapter3/3-6.cc
@@ -13,9 +13,17 @@ struct twice2 {
 
 int main()
 {
+    typedef typename mpl::lambda<
+                        mpl::apply<_1, 
+                            typename mpl::apply<_1, _2>::type
+                                >::type
+                                    >::type twice3;
+    typedef typename mpl::lambda<_1::template apply<_1::template apply<_2>::type>::type>::type twice4;
     
     static_assert(std::is_same<twice<std::add_pointer<_1>, int>::type, int**>::value, "twice");
     
-    static_assert(std::is_same<mpl::apply<twice2, std::add_pointer<_1>, int>::type, int**>::value, "shit");
+    static_assert(std::is_same<mpl::apply<twice2, std::add_pointer<_1>, int>::type, int**>::value, "metafunction class");
+    static_assert(std::is_same<mpl::apply<twice3, std::add_pointer<_1>, int>::type, int**>::value, "placeholder expression");
+    static_assert(std::is_same<mpl::apply<twice4, std::add_pointer<_1>, int>::type, int**>::value, "placeholder expression");
     return 0;
 }
--- chapter3/3-7.cc
+++ chapter3/3-7.cc
@@ -13,6 +13,8 @@ int main()
     
     /* the second parameter */
     typedef mpl::apply<_1, mpl::plus<_1, _2> >::type t2;
+    // equal to : 
+    // typedef _1::template apply<mpl::plus<_1, _2>>::type t2; 
     static_assert(std::is_same<t2, mpl::plus<_1, _2>>::value, "t2");
 
     typedef mpl::apply<_1, std::vector<int>> ::type t3;
@@ -21,20 +23,22 @@ int main()
     typedef mpl::apply<_1, std::vector<_1> >::type t4;
     static_assert(std::is_same<t4, std::vector<_1> >::value, "t4");
 
+    // equal to apply<_1, ...>, because mpl::apply could accept both a placeholder, or a metafunction class
     typedef mpl::apply<mpl::lambda<_1>, std::vector<int> >::type t5;
     static_assert(std::is_same<t5, std::vector<int> >::value, "t5");
 
     /* metafunction class, same as the second parameter */
     typedef mpl::apply<mpl::lambda<_1>, std::vector<_1> >::type t6;
-    static_assert(std::is_same<t6::template apply<int>::type, std::vector<int> >::value, "t6");
+    static_assert(std::is_same<mpl::apply<t6, int>::type, std::vector<int> >::value, "t6");
     
     /* metafunction class, which needs two parameter */
     typedef mpl::apply<mpl::lambda<_1>, mpl::plus<_1, _2> >::type t7;
-    static_assert(mpl::plus<mpl::int_<1>, mpl::int_<2> >::type::value == 3, "1+2 =? 3");
-    static_assert(t7::template apply<mpl::int_<1>, mpl::int_<2>>::type::value == 3, "t7");
+    static_assert(std::is_same<t7, mpl::lambda<mpl::plus<_1, _2>>::type>::value, "t7");
 
     /* same as the second parameter */
+    // mpl::lambda<...>, construct a metafunction class, but without take the result
     typedef mpl::apply<_1, mpl::lambda<mpl::plus<_1, _2> > > ::type t8;
+    static_assert(std::is_same<t8, mpl::lambda<mpl::plus<_1, _2> > >::value, "t8");
     static_assert(t8::type::template apply<mpl::int_<1>, mpl::int_<2>>::type::value == 3, "t8");
 
     puts("pass all assert!");
