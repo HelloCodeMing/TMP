8cfe23e54d4472611b732ff1888e6c3d3c163caf
HelloCodeMing <huanmingwong@163.com>
2015-06-05 12:58:38 +0800

    logical or

--- chapter4/logical_or.hpp
+++ chapter4/logical_or.hpp
@@ -1,27 +1,23 @@
 #include "common-header.hpp"
 
-template <class T1 = mpl::false_, class T2 = mpl::false_, class T3 = mpl::false_, class T4 = mpl::false_>
-struct logical_or {
-    typedef mpl::false_ type;
-    const static bool value = false;
+using mpl::true_;
+using mpl::false_;
+
+template <class ...Arg> struct logical_or {};
+
+template <>
+struct logical_or<> {
+    typedef false_ type;
+    static const bool value = false;
 };
 
-template <class T2, class T3, class T4>
-struct logical_or <mpl::true_, T2, T3, T4> {
-    typedef mpl::true_ type;
-    const static bool value = true;
-};
-
-template <class T3, class T4>
-struct logical_or <mpl::false_, mpl::true_, T3, T4> {
-    typedef mpl::true_ type;
-    const static bool value = true;
-};
-
-template <class T4>
-struct logical_or <mpl::false_, mpl::false_, mpl::true_, T4> {
-    typedef mpl::true_ type;
-    const static bool value = true;
+template <class Head, class ...Tail>
+struct logical_or<Head, Tail...> {
+    typedef typename mpl::eval_if< Head,
+                          true_,
+                          logical_or<Tail...>
+                        >::type type;
+    static const bool value = true;
 };
 
 
@@ -36,45 +32,45 @@ template <class T>
 struct Boom {
 };
 
-/* The mpl::or_ */
-static_assert(mpl::or_< mpl::true_,
+/* The or_ */
+static_assert(mpl::or_< true_,
                         mpl::identity<int&>,
                         Boom<int> 
                         >::value, "short circuit or boom");
 
 static_assert(std::is_same< 
-                mpl::or_< mpl::true_, mpl::false_>::type, 
-                mpl::true_>::value, "true_ and false_");
+                mpl::or_< true_, false_>::type, 
+                true_>::value, "true_ and false_");
 
 static_assert(std::is_same<
-                mpl::or_< mpl::false_, mpl::true_>::type,
-                mpl::true_>::value, "false_ and true_");
+                mpl::or_< false_, true_>::type,
+                true_>::value, "false_ and true_");
 
 static_assert(std::is_same<
-                mpl::or_< mpl::false_, mpl::false_>::type,
-                mpl::false_>::value, "true_ and true_");
+                mpl::or_< false_, false_>::type,
+                false_>::value, "true_ and true_");
 
 /* logical or */
-static_assert(logical_or< mpl::true_,
-                        Boom<int>
+static_assert(logical_or< true_,
+                          Boom<int>
                         >::value, "short circuit or boom");
 
 static_assert(std::is_same<
-                logical_or< mpl::true_, mpl::false_>::type,
-                mpl::true_>::value, "true_ and false_");
+                logical_or< true_, false_>::type,
+                true_>::value, "true_ and false_");
 
 static_assert(std::is_same<
-                logical_or< mpl::false_, mpl::true_>::type,
-                mpl::true_>::value, "false_ and true_");
+                logical_or< false_, true_>::type,
+                true_>::value, "false_ and true_");
 
 static_assert(std::is_same<
-                logical_or< mpl::false_, mpl::false_>::type,
-                mpl::false_>::value, "false_ and false_");
+                logical_or< false_, false_>::type,
+                false_>::value, "false_ and false_");
 
 static_assert(std::is_same<
-                logical_or< mpl::false_, mpl::false_, mpl::false_, mpl::true_>::type,
-                mpl::false_>::value, "false_ ,false_, false_, true_");
+                logical_or< false_, false_, false_, true_>::type,
+                true_>::value, "false_ ,false_, false_, true_");
 
 static_assert(std::is_same<
-                logical_or< mpl::false_, mpl::false_, mpl::false_, mpl::false_>::type,
-                mpl::false_>::value, "false_, false_, false_, false_");
+                logical_or< false_, false_, false_, false_>::type,
+                false_>::value, "false_, false_, false_, false_");
